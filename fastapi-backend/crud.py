37a685730a543ff7aec087e4171f9af3a53cd505
bilal-faisal <bilalfaisal.developer@gmail.com>
2024-01-10 21:16:56 +0500

    user_id instead of token

--- fastapi-backend/crud.py
+++ fastapi-backend/crud.py
@@ -7,71 +7,74 @@ app : FastAPI = FastAPI()
 def welcome() -> dict:
     return {"message" : "Welcome to Todo API"}
 
-# Generate token
-@app.post("/token")
-def generate_token() -> dict:
-    # Generate token and store it in database
-    return {"token" : "token"}
+
+# Generate user_id
+@app.post("/user_id")
+def generate_user_id() -> dict:
+    # Generate user_id and store it in database
+    return {"user_id" : ""}
+
 
 # Get all todos
 @app.get("/todos")
-def get_todos(token: str = Header(default=None)) -> dict:
-    if token:
-        # validate whether token exists in database
-        exists:bool = False
+def get_todos(user_id: str = Header(default=None)) -> dict:
+    if user_id:
+        # validate whether user_id exists in database
+        exists:bool = True
         if exists:
-            # get all todos on the basis of token
+            # get all todos on the basis of user_id
             todos : list[dict] = []
-            return {"status": "success", "todos" : todos, "token" : token}
+            return {"status": "success", "todos" : todos, "user_id" : user_id}
         
-        return {"status": "error", "message" : "Invalid token"}
+        return {"status": "error", "message" : "Invalid user_id"}
     else:
         # if not exists, return error message
-        return {"status": "error", "message" : "Token not provided"}
+        return {"status": "error", "message" : "user_id not provided"}
 
 
 # Post a todo
 @app.post("/todo")
-def post_todo(todo:dict = Body(embed=True), token: str = Header(default=None)):
-    if token:
-        # Validate whether token exists in database
+def post_todo(todo:dict = Body(embed=True), user_id: str = Header(default=None)):
+    if user_id:
+        # Validate whether user_id exists in database
         exists:bool = True
         if exists:
-            # post the todo on the basis of token
-            return {"message" : "Todo Posted", "todo" : todo, "token" : token}
+            # post the todo on the basis of user_id
+            return {"message" : "Todo Posted", "todo" : todo, "user_id" : user_id}
         
-        return {"status": "error", "message" : "Invalid token"}
+        return {"status": "error", "message" : "Invalid user_id"}
     else:
         # if not exists, return error message
-        return {"status": "error", "message" : "Token not provided"}
+        return {"status": "error", "message" : "user_id not provided"}
 
 
 # Delete a todo
 @app.delete("/todo")
-def delete_todo(todo_id:int = Body(embed=True), token: str = Header(default=None)):
-    if token:
-        # Validate whether token exists in database
+def delete_todo(todo_id:int = Body(embed=True), user_id: str = Header(default=None)):
+    if user_id:
+        # Validate whether user_id exists in database
         exists:bool = True
         if exists:
-            # delete the todo on the basis of token and todo_id
-            return {"message" : "Todo Deleted", "todo_id" : todo_id, "token" : token}
+            # delete the todo on the basis of user_id and todo_id
+            return {"message" : "Todo Deleted", "todo_id" : todo_id, "user_id" : user_id}
         
-        return {"status": "error", "message" : "Invalid token"}
+        return {"status": "error", "message" : "Invalid user_id"}
     else:
         # if not exists, return error message
-        return {"status": "error", "message" : "Token not provided"}
+        return {"status": "error", "message" : "user_id not provided"}
+
 
 # Update a todo
 @app.patch("/todo")
-def update_todo(todo_id:int = Body(), todo:dict = Body(), token: str = Header(default=None)):
-    if token:
-        # Validate whether token exists in database
+def update_todo(todo_id:int = Body(), todo:dict = Body(), user_id: str = Header(default=None)):
+    if user_id:
+        # Validate whether user_id exists in database
         exists:bool = True
         if exists:
-            # update the todo on the basis of token and todo_id
-            return {"message" : "Todo Updated", "todo_id" : todo_id, "todo" : todo, "token" : token}
+            # update the todo on the basis of user_id and todo_id
+            return {"message" : "Todo Updated", "todo_id" : todo_id, "todo" : todo, "user_id" : user_id}
         
-        return {"status": "error", "message" : "Invalid token"}
+        return {"status": "error", "message" : "Invalid user_id"}
     else:
         # if not exists, return error message
-        return {"status": "error", "message" : "Token not provided"}
+        return {"status": "error", "message" : "user_id not provided"}
