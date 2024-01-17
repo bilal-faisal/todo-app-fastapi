e2112d6cd1fee2707cb2a9ac16e12fe01192de0c
bilal-faisal <bilalfaisal.developer@gmail.com>
2024-01-17 16:08:56 +0500

    Added tests for /todo and /todo/n

--- tests/test_main.py
+++ tests/test_main.py
@@ -9,10 +9,14 @@ def random_with_N_digits(n):
     range_end = (10**n)-1
     return randint(range_start, range_end)
 
+# GET/
+
 def test_base_route():
     res = client.get('/')
     assert res.status_code == 200
     assert res.json() == {'message': 'Welcome to Todo API'}
+
+# POST/user
 
 def test_add_new_user_success():
     response = client.post("/user", json={"name": "Test User", "email": f"{random_with_N_digits(10)}@test.com"})
@@ -43,3 +47,119 @@ def test_add_new_user_invalid_email():
     response = client.post("/user", json={"name": "Test User", "email": "invalid_email"})
     assert response.json()["status"] == "error"
     assert response.json()["message"] == "Invalid email"
+
+# GET/todo/n
+
+def test_get_single_todo_missing_parameters():
+    response = client.get("/todo/4")
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["type"] == "missing"
+
+def test_get_single_todo_invalid_user_id():
+    response = client.get("/todo/4", headers={"user-id":"invalid"})
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["type"] == "int_parsing"
+
+def test_get_single_todo_user_id_not_exists():
+    response = client.get("/todo/4", headers={"user-id":"10000000"})
+    assert response.json()["status"] == "error"
+    assert response.json()["message"] == "User does not exists"
+
+def test_get_single_task_success():
+    response = client.get("/todo/4", headers={"user-id":"20"})
+    assert response.status_code == 200
+    assert response.json()["status"] == "success"
+    assert "todos" in response.json()
+
+def test_get_single_todo_id_not_exists():
+    response = client.get("/todo/1000000", headers={"user-id":"20"})
+    assert response.json()["status"] == "error"
+    assert response.json()["message"] == "Todo not found"
+
+# GET/todos
+
+def test_get_all_todos_success():
+    response = client.get("/todos", headers={"user-id":"20"})
+    assert response.status_code == 200
+    assert response.json()["status"] == "success"
+    assert "todos" in response.json()
+
+def test_get_all_todos_invalid_user_id():
+    response = client.get("/todos", headers={"user-id":"invalid"})
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["type"] == "int_parsing"
+
+def test_get_all_todos_user_id_not_exists():
+    response = client.get("/todos", headers={"user-id":"10000000"})
+    assert response.json()["status"] == "error"
+    assert response.json()["message"] == "User does not exists"
+
+def test_get_all_todos_missing_user_id():
+    response = client.get("/todos")
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["type"] == "missing"
+
+# POST/todo
+
+def test_add_new_todo_success():
+    response = client.post("/todo", json={"title": "Test Todo", "description": "This is a test todo"}, headers={"user-id":"20"})
+    assert response.status_code == 200
+    assert response.json()["status"] == "success"
+    assert "todo_id" in response.json()
+
+def test_add_new_todo_invalid_user_id():
+    response = client.post("/todo", json={"title": "Test Todo", "description": "This is a test todo"}, headers={"user-id":"invalid"})
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["type"] == "int_parsing"
+
+def test_add_new_todo_user_id_not_exists():
+    response = client.post("/todo", json={"title": "Test Todo", "description": "This is a test todo"}, headers={"user-id":"10000000"})
+    assert response.json()["status"] == "error"
+    assert response.json()["message"] == "User does not exists"
+
+def test_add_new_todo_missing_parameters():
+    response = client.post("/todo", json={}, headers={"user-id":"20"})
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["type"] == "missing"
+
+def test_add_new_todo_missing_user_id():
+    response = client.post("/todo", json={"title": "Test Todo", "description": "This is a test todo"})
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["type"] == "missing"
+
+# PATCH/todo
+
+def test_update_todo_success():
+    response = client.patch("/todo", json={
+        "title": "Updated Todo",
+        "todo_id": 10
+        }, headers={"user-id":"20"})
+    assert response.status_code == 200
+    assert response.json()["status"] == "success"
+    assert "todo_id" in response.json()
+
+def test_update_todo_missing_both_parameters():
+    response = client.patch("/todo", json={"todo_id": 10}, headers={"user-id":"20"})
+    assert response.json()["status"] == "error"
+    assert response.json()["message"] == "Both title and description cannot be empty"
+
+def test_update_todo_todo_id_not_exists():
+    response = client.patch("/todo", json={"title": "Updated Todo"}, headers={"user-id":"20"})
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["type"] == "missing"
+
+def test_update_todo_user_id_not_exists():
+    response = client.patch("/todo", json={"title": "Updated Todo", "todo_id": "10"})
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["type"] == "missing"
+
+def test_update_todo_invalid_user_id():
+    response = client.patch("/todo", json={"title": "Updated Todo", "todo_id": "10"}, headers={"user-id":"invalid"})
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["type"] == "int_parsing"
+
+def test_update_todo_invalid_todo_id():
+    response = client.patch("/todo", json={"title": "Updated Todo", "todo_id": "invalid"}, headers={"user-id":"20"})
+    assert response.status_code == 422
+    assert response.json()["detail"][0]["type"] == "int_parsing"
+
